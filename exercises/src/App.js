1b5211f5a28fcfa5ac06d43ca90e659798d2d1a9
plamen <plamen91ivanov@abv.bg>
2021-04-28 15:24:51 +0300

    add

---
+++ exercises/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hgelll</h1>
+      <button>Increment</button>
+    </div>
+  );
+}
+
+export default App;
