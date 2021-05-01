84e6ae7584417abec17700dbf44d153f190a8a17
plamen <plamen91ivanov@abv.bg>
2021-05-01 17:21:12 +0300

    state

--- exercises/src/components/counter.jsx
+++ exercises/src/components/counter.jsx
@@ -3,20 +3,18 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        tags: []
     };
 
-    renderTags() {
-        if (this.state.tags.length ===0) return <p>There no tag6s!</p>;
-
-        return <ul> {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}  </ul>
-
+    handleIncrement = () => {
+        console.log('count');
+        this.setState({count: this.state.count + 1})
     }
  
     render() {
         return (
             <div>
-                {this.renderTags()}
+              <span className={this.getBadgeClases()}>{this.formatCount()}</span>
+              <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
             </div>
           );
     }
